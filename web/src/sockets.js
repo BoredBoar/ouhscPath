635ea9b7c646c90c0d16f0fdbc226aab64004365
CWILL17 <cwill17@oupbmsbhtdd.hsc.net.ou.edu>
2017-08-11 12:59:19 -0500

    cleaning up, add knockout

--- web/src/sockets.js
+++ web/src/sockets.js
@@ -2,69 +2,10 @@ module.exports = function(io,db) {
   io.on('connection', function(socket){
     console.log('a user connected');
 
+    socket.emit('my other event', 'thist is a test');
+
     socket.on('test', function(msg) {
       console.log('test message recieved: ' + msg);
-    });
-
-    socket.on('get calendar',function(data) {
-      console.log(data);
-      events = [
-        {
-          title: 'All Day Event',
-          start: '2017-05-01'
-        },
-        {
-          title: 'Long Event',
-          start: '2017-05-07',
-          end: '2017-05-10'
-        },
-        {
-          id: 999,
-          title: 'Repeating Event',
-          start: '2017-05-09T16:00:00'
-        },
-        {
-          id: 999,
-          title: 'Repeating Event',
-          start: '2017-05-16T16:00:00'
-        },
-        {
-          title: 'Conference',
-          start: '2017-05-11',
-          end: '2017-05-13'
-        },
-        {
-          title: 'Meeting',
-          start: '2017-05-12T10:30:00',
-          end: '2017-05-12T12:30:00'
-        },
-        {
-          title: 'Lunch',
-          start: '2017-05-12T12:00:00'
-        },
-        {
-          title: 'Meeting',
-          start: '2017-05-12T14:30:00'
-        },
-        {
-          title: 'Happy Hour',
-          start: '2017-05-12T17:30:00'
-        },
-        {
-          title: 'Dinner',
-          start: '2017-05-12T20:00:00'
-        },
-        {
-          title: 'Birthday Party',
-          start: '2017-05-13T07:00:00'
-        },
-        {
-          title: 'Click for Google',
-          url: 'http://google.com/',
-          start: '2017-05-28'
-        }
-      ];
-      socket.emit('calendar events', {events:events});
     });
 
   });
